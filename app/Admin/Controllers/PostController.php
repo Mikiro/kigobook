f198bb5e6b82c228596f2ff46fdfad212d279d35
Mikiro <youyouyouapple@yahoo.co.jp>
2019-09-19 12:43:09 +0000

    first commit

---
+++ app/Admin/Controllers/PostController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Post;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class PostController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Post';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Post);
+
+        $grid->column('id', __('Id'));
+        $grid->column('content_upper', __('Content upper'));
+        $grid->column('content_middle', __('Content middle'));
+        $grid->column('content_bottom', __('Content bottom'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Post::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('content_upper', __('Content upper'));
+        $show->field('content_middle', __('Content middle'));
+        $show->field('content_bottom', __('Content bottom'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Post);
+
+        $form->textarea('content_upper', __('Content upper'));
+        $form->textarea('content_middle', __('Content middle'));
+        $form->textarea('content_bottom', __('Content bottom'));
+
+        return $form;
+    }
+}
