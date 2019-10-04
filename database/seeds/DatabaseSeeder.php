538588dbe23545b10cdfcb571df8a2c56f488742
Mikiro <youyouyouapple@yahoo.co.jp>
2019-10-04 08:01:07 +0000

    make it better

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PostsTableSeeder::class);
-        $this->call(AdminTableSedder::class);
+        $this->call(AdminTableSeeder::class);
     }
 }
