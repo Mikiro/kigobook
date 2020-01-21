2922385bf2c766faa0611d21f528fe8f4fec9e38
Mikiro <youyouyouapple@yahoo.co.jp>
2020-01-21 13:54:46 +0000

    height 100%;

--- resources/views/posts/kigobook.blade.php
+++ resources/views/posts/kigobook.blade.php
@@ -6,7 +6,7 @@
            
                 <div class="row h-100">
                     @foreach($posts as $post)
-                    <div class="table-responsive">
+                    <div class="">
                         <table class="border-left">
                             <tr class="h-75">
                                 <td class="text-nowrap"><h2 style="height: 520px;">{{$post->content_upper}}{{$post->content_middle}}{{$post->content_bottom}}</h></td>
