79737ad024623879a53313b94ffa256de0026bbf
Mikiro <youyouyouapple@yahoo.co.jp>
2020-05-28 22:10:49 +0000

    change each season's index format

--- resources/views/posts/winterWordPost.blade.php
+++ resources/views/posts/winterWordPost.blade.php
@@ -14,32 +14,28 @@
           -moz-column-gap: 10;
           -ms-column-gap: 10;
 
-          line-height: 3.9;
+          line-height: 2.9;
           letter-spacing: 0.06em;
           -webkit-writing-mode: vertical-rl;
           -ms-writing-mode: tb-rl;
           -epubg-writing-mode: tb-rl;
-          writing-mode: tb-rl;
-          text-indent: 1em;
+          /*writing-mode: tb-rl;*/
+          text-indent: 0.7em;
           /*margin-left: 30px;*/
           
           /*font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Yu Gothic", YuGothic, Verdana, Meiryo, "M+ 1p", sans-serif;*/
           right: 0;
           
           font-size:1.6rem;/* 16px*/
+          display: block;
           
      }
      
-     .vertical h2 {
-         font-size: 24px;
-         font-size: calc(2rem + ((1vw - 0.64rem) * 0.7143));/* 20px~24pxで可変*/
-         line-height: 1.3;
-     }
-     
-     .vertical h3 {
-         font-size: 20px;
-         font-size: calc(2rem + ((1vw - 0.64rem) * 0.7143));/* 20px~24pxで可変*/
-         line-height: 1.3;
+    .vertical td {
+       
+         font-size: calc(2.0rem + ((1vw - 0.54rem) * 0.5143));/* 20px~24pxで可変*/
+         /*line-height: 1.3;*/
+         font-weight: normal;
      }
      
      .text-combine{
@@ -48,34 +44,50 @@
         -ms-text-combine-horizontal: all;
     }
     
+      @media screen and (max-height: 823px) {
+   /* 縦幅が823px以下の場合に適用するスタイル */
+       .row{height:500px!important;}
+    }
+    
+    @media screen and (max-height: 568px) {
+   /* 縦幅が568px以下の場合に適用するスタイル */
+       .row{height:400px!important;}
+    }
+    
     </style>
     
-    <div class="vertical mw-100 overflow-auto mx-auto">
-        <div class="row" style="margin-right: 15px;">
-        @foreach($winterWordPosts as $winterWordPost)
-            <div class="border-left" style="height: 500px; margin-right: 15px;">
-                <table>
-                    <tr>
-                        <td class="text-nowrap"><h3 class="mt-0 mb-0" style="height: 80px;">{{$winterWordPost->name}}</h3></td>
-                    </tr>
-                    @foreach($winterWordPost->posts as $post )
-                    <tr>
-                        <td class="text-nowrap"><h2 class="mt-0 mb-0" style="height: 430px;">{{ $post->content_upper}}{{ $post->content_middle}}{{ $post->content_bottom}}</h2></td>
-                    </tr>
-                </table>
-                <table>
-                    <tr>
-                        <td><h2 class="text-nowrap" style="margin-top: 150px; height: 170px;">{{ $post->author}}</h2></td>
-                        <td><h2 class="text-nowrap" style="height: 40px margin-top: 10px">{{$post->date}}</h2></td>
-                        <td><h2 class="text-nowrap text-combine" style="height: 35px; margin-top: 0px">{{$post->year}}</h2></td>
-                    </tr>
-                </table>
-                    @endforeach
-            </div>
-        @endforeach
+    <div class="vertical mw-100 overflow-auto panel panel-default panel-warning mx-auto">
+     
+         <div class="row" style="height:640px;">
+             
+            @foreach($winterWordPosts as $winterWordPost)
+                <div class="" style=" margin-right: 15px;">
+                    <table class="">
+                        <tr>
+                            <td class="text-nowrap" style="height: 100%; line-height: 3.5; font-weight:800; margin-left: 25px;">{{$winterWordPost->name}}</td>
+                        </tr>
+                         @foreach($winterWordPost->posts as $post )
+                         
+                        <table class="table-hover">
+                            <tr>
+                                <td class="text-nowrap" height="630">{{$post->content_upper}}{{$post->content_middle}}{{$post->content_bottom}}</td>
+                            </tr>
+                        </table>
+                        <table class="table-hover border-left">
+                            <tr>
+                                <td height="277"></td>
+                                <td height="277"class="text-nowrap">{{ $post->author}}</td>
+                                <td height="66" class="text-nowrap">{{$post->date}} <span class="text-combine">{{$post->year}}</span> </td>
+                            </tr>
+                        </table>
+                       
+                         @endforeach
+                    </table>
+                </div>
+            @endforeach
         </div>
     </div>
-    
+ 
 @endsection
 
 
