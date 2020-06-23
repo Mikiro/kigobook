46c214d4270c79ad9d2f190e1e02a38a6277c700
Mikiro <youyouyouapple@yahoo.co.jp>
2020-06-23 21:52:16 +0000

    add link to homeback on

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -8,3 +8,7 @@
 
 @section('detail', 'It will be returned when there is a syntax error in the program, or there is an error in the setting. Please contact the administrator.')
 {{-- プログラムに文法エラーがあったり、設定に誤りがあった場合などに返されます。管理者へ連絡してください。 --}}
+
+@section('link')
+  <p><a href="/">to TOP&gt;&gt;</a></p>
+@endsection
