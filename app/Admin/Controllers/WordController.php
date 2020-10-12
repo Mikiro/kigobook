8cc1f792c8aecbd5c7ba1b986e91618948c9aa8a
Mikiro <youyouyouapple@yahoo.co.jp>
2020-10-12 22:06:50 +0000

    test

--- app/Admin/Controllers/WordController.php
+++ app/Admin/Controllers/WordController.php
@@ -83,21 +83,9 @@ class WordController extends AdminController
         $form->textarea('explain', __('explain'));
         $form->image('photo', 'Photo')->removable();
         $form->select('category_id', 'Category')->options($categories);
+        Storage::disk('s3')->putFile('photos', new File('/path/to/photo'));
 
         return $form;
     }
     
-    public function upload(Request $request)
-    {
-    $file = $request->file('file');
-    // 第一引数はディレクトリの指定
-    // 第二引数はファイル
-    // 第三引数はpublickを指定することで、URLによるアクセスが可能となる
-    $path = Storage::disk('s3')->putFile('/', $file, 'public');
-    // hogeディレクトリにアップロード
-    // $path = Storage::disk('s3')->putFile('/hoge', $file, 'public');
-    // ファイル名を指定する場合はputFileAsを利用する
-    // $path = Storage::disk('s3')->putFileAs('/', $file, 'hoge.jpg', 'public');
-    return redirect('/');
-    }
-}
+ }
