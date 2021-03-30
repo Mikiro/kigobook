2a8602731a96968ff1b415301095b6975adb7757
Mikiro <youyouyouapple@yahoo.co.jp>
2021-03-30 22:18:06 +0000

    a

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,11 +46,11 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    // public function render($request, Exception $exception)
-    // {
+    public function render($request, Exception $exception)
+    {
         
-    //     return parent::render($request, $exception);
-    // }
+        return parent::render($request, $exception);
+    }
     
 //     public function render($request, Exception $e) {
 
